a3b5292755865aa5ac00b0111a83c9c99512e821
santigc6 <santiago.gonzalez-carvajal@estudiante.uam.es>
2017-12-08 17:43:06 +0100

    fixed versions' problem

--- Pr4/materialP4/ejercicio2.sh
+++ Pr4/materialP4/ejercicio2.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
+make
+rm -f ej2/*
+i=0
+max_tam=2010000000
+min_tam=6500000
+tam=0
+incr=$(((max_tam-min_tam)/10))
+max_hilos=4
+n_hilos=0
+reps=5
+for((i = 0; i < $reps; i++)); do
+	echo "Repeticion $i"
+	for((tam=$min_tam; tam < $max_tam; tam += $incr)); do
+		echo "	Tamanio $tam"
+		./pescalar_serie $tam | grep "Tiempo" | awk -v t="$tam" '{print t" "$2}' >> ej2/pesc_serie.dat
+		for((n_hilos = 0; n_hilos < $max_hilos; n_hilos++)); do
+			./pescalar_par2 $tam $n_hilos | grep "Tiempo" | awk -v t="$tam" '{print t" "$2}' >> ej2/pesc_paralelo_$n_hilos.dat 
+		done
+	done
+done
+
+awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_serie.dat | sort -nk1 > ej2/pesc_serie_media.dat
+awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_0.dat | sort -nk1 > ej2/pesc_paralelo_0_media.dat
+awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_1.dat | sort -nk1 > ej2/pesc_paralelo_1_media.dat
+awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_2.dat | sort -nk1 > ej2/pesc_paralelo_2_media.dat
+awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_3.dat | sort -nk1 > ej2/pesc_paralelo_3_media.dat
+
+rm -f ej2/pesc_serie.dat ej2/pesc_paralelo_0.dat ej2/pesc_paralelo_1.dat ej2/pesc_paralelo_2.dat ej2/pesc_paralelo_3.dat
+
 paste ej2/pesc_serie_media.dat ej2/pesc_paralelo_0_media.dat ej2/pesc_paralelo_1_media.dat ej2/pesc_paralelo_2_media.dat ej2/pesc_paralelo_3_media.dat > ej2/aux.dat
 
 awk '{print $1" "$2/$2" "$2/$4" "$2/$6" "$2/$8" "$2/$10}' ej2/aux.dat > ej2/speedups.dat
+
+cd ej2
 
 gnuplot << END_GNUPLOT
 set title "Aceleracion"
@@ -11,11 +42,11 @@ set key right bottom
 set grid
 set term png
 set output "speedup.png"
-plot "ej2/speedups.dat" using 1:2 with lines lw 2 title "p_serie", \
-     "ej2/speedups.dat" using 1:3 with lines lw 2 title "p_par_1", \
-     "ej2/speedups.dat" using 1:4 with lines lw 2 title "p_par_2", \
-     "ej2/speedups.dat" using 1:5 with lines lw 2 title "p_par_3", \
-     "ej2/speedups.dat" using 1:6 with lines lw 2 title "p_par_4"
+plot "speedups.dat" using 1:2 with lines lw 2 title "p_serie", \
+     "speedups.dat" using 1:3 with lines lw 2 title "p_par_1", \
+     "speedups.dat" using 1:4 with lines lw 2 title "p_par_2", \
+     "speedups.dat" using 1:5 with lines lw 2 title "p_par_3", \
+     "speedups.dat" using 1:6 with lines lw 2 title "p_par_4"
 replot
 quit
 END_GNUPLOT
@@ -28,13 +59,15 @@ set key right bottom
 set grid
 set term png
 set output "times.png"
-plot "ej2/pesc_serie_media.dat" using 1:2 with lines lw 2 title "p_serie", \
-     "ej2/pesc_paralelo_0_media.dat" using 1:2 with lines lw 2 title "p_par_1", \
-     "ej2/pesc_paralelo_1_media.dat" using 1:2 with lines lw 2 title "p_par_2", \
-     "ej2/pesc_paralelo_2_media.dat" using 1:2 with lines lw 2 title "p_par_3", \
-     "ej2/pesc_paralelo_3_media.dat" using 1:2 with lines lw 2 title "p_par_4"
+plot "pesc_serie_media.dat" using 1:2 with lines lw 2 title "p_serie", \
+     "pesc_paralelo_0_media.dat" using 1:2 with lines lw 2 title "p_par_1", \
+     "pesc_paralelo_1_media.dat" using 1:2 with lines lw 2 title "p_par_2", \
+     "pesc_paralelo_2_media.dat" using 1:2 with lines lw 2 title "p_par_3", \
+     "pesc_paralelo_3_media.dat" using 1:2 with lines lw 2 title "p_par_4"
 replot
 quit
 END_GNUPLOT
+
+cd ..
 
 make clean
