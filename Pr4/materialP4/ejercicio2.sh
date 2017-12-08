c6c373703c4802f2f66dac529d8f8a6a69983f07
santigc6 <santiago.gonzalez-carvajal@estudiante.uam.es>
2017-12-08 17:53:27 +0100

    fixed threads' problem

--- Pr4/materialP4/ejercicio2.sh
+++ Pr4/materialP4/ejercicio2.sh
@@ -2,8 +2,8 @@
 make
 rm -f ej2/*
 i=0
-max_tam=2010000000
-min_tam=6500000
+max_tam=800000000
+min_tam=100000000
 tam=0
 incr=$(((max_tam-min_tam)/10))
 max_hilos=4
@@ -11,24 +11,24 @@ n_hilos=0
 reps=5
 for((i = 0; i < $reps; i++)); do
 	echo "Repeticion $i"
-	for((tam=$min_tam; tam < $max_tam; tam += $incr)); do
+	for((tam=$min_tam; tam <= $max_tam; tam += $incr)); do
 		echo "	Tamanio $tam"
 		./pescalar_serie $tam | grep "Tiempo" | awk -v t="$tam" '{print t" "$2}' >> ej2/pesc_serie.dat
-		for((n_hilos = 0; n_hilos < $max_hilos; n_hilos++)); do
+		for((n_hilos = 1; n_hilos <= $max_hilos; n_hilos++)); do
 			./pescalar_par2 $tam $n_hilos | grep "Tiempo" | awk -v t="$tam" '{print t" "$2}' >> ej2/pesc_paralelo_$n_hilos.dat 
 		done
 	done
 done
 
-awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_serie.dat | sort -nk1 > ej2/pesc_serie_media.dat
-awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_0.dat | sort -nk1 > ej2/pesc_paralelo_0_media.dat
-awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_1.dat | sort -nk1 > ej2/pesc_paralelo_1_media.dat
-awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_2.dat | sort -nk1 > ej2/pesc_paralelo_2_media.dat
-awk -v "n=$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_3.dat | sort -nk1 > ej2/pesc_paralelo_3_media.dat
+awk -v n="$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_serie.dat | sort -nk1 > ej2/pesc_serie_media.dat
+awk -v n="$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_1.dat | sort -nk1 > ej2/pesc_paralelo_1_media.dat
+awk -v n="$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_2.dat | sort -nk1 > ej2/pesc_paralelo_2_media.dat
+awk -v n="$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_3.dat | sort -nk1 > ej2/pesc_paralelo_3_media.dat
+awk -v n="$reps" '{valores[$1]+=$2} END{for(i in valores) print i" "valores[i]/n}' ej2/pesc_paralelo_4.dat | sort -nk1 > ej2/pesc_paralelo_4_media.dat
 
-rm -f ej2/pesc_serie.dat ej2/pesc_paralelo_0.dat ej2/pesc_paralelo_1.dat ej2/pesc_paralelo_2.dat ej2/pesc_paralelo_3.dat
+rm -f ej2/pesc_serie.dat ej2/pesc_paralelo_1.dat ej2/pesc_paralelo_2.dat ej2/pesc_paralelo_3.dat ej2/pesc_paralelo_4.dat
 
-paste ej2/pesc_serie_media.dat ej2/pesc_paralelo_0_media.dat ej2/pesc_paralelo_1_media.dat ej2/pesc_paralelo_2_media.dat ej2/pesc_paralelo_3_media.dat > ej2/aux.dat
+paste ej2/pesc_serie_media.dat ej2/pesc_paralelo_1_media.dat ej2/pesc_paralelo_2_media.dat ej2/pesc_paralelo_3_media.dat ej2/pesc_paralelo_4_media.dat > ej2/aux.dat
 
 awk '{print $1" "$2/$2" "$2/$4" "$2/$6" "$2/$8" "$2/$10}' ej2/aux.dat > ej2/speedups.dat
 
@@ -60,10 +60,10 @@ set grid
 set term png
 set output "times.png"
 plot "pesc_serie_media.dat" using 1:2 with lines lw 2 title "p_serie", \
-     "pesc_paralelo_0_media.dat" using 1:2 with lines lw 2 title "p_par_1", \
-     "pesc_paralelo_1_media.dat" using 1:2 with lines lw 2 title "p_par_2", \
-     "pesc_paralelo_2_media.dat" using 1:2 with lines lw 2 title "p_par_3", \
-     "pesc_paralelo_3_media.dat" using 1:2 with lines lw 2 title "p_par_4"
+     "pesc_paralelo_1_media.dat" using 1:2 with lines lw 2 title "p_par_1", \
+     "pesc_paralelo_2_media.dat" using 1:2 with lines lw 2 title "p_par_2", \
+     "pesc_paralelo_3_media.dat" using 1:2 with lines lw 2 title "p_par_3", \
+     "pesc_paralelo_4_media.dat" using 1:2 with lines lw 2 title "p_par_4"
 replot
 quit
 END_GNUPLOT
